2579bf44725159c1ad0f6f9cba2720343f9aff4a
Ali Hassani <alih@gatech.edu>
2024-03-26 15:40:32 -0400

    Update docs, and WindowsBuilder

--- WindowsBuilder.bat
+++ WindowsBuilder.bat
@@ -1,21 +1,50 @@
-@ECHO off    
-if /I %1 == default goto :default
-if /I %1 == install goto :install
-if /I %1 == clean goto :clean
+@ECHO off
 
-goto :eof ::can be ommited to run the `default` function similarly to makefiles
+if [%1] == [test] shift & goto :test
+if [%1] == [install] shift & goto :install
+if [%1] == [clean] shift & goto :clean
 
-:default
-echo "Please specify a target."
+:parseArgs
+if [%1] == [WORKERS] set NATTEN_N_WORKERS=%2 & shift & shift & goto :parseargs
+if [%1] == [CUDA_ARCH] set NATTEN_CUDA_ARCH=%2 & shift & shift & goto :parseargs
+if [%1] == [VERBOSE] set NATTEN_VERBOSE=%2 & shift & shift & goto :parseargs
+goto :installContinue
+:end
+
+:test
+echo "Testing NATTEN"
+pip install -r requirements-dev.txt
+pytest -v -x ./tests
 goto :eof
+:end
 
 :install
-echo Building NATTEN from source
-cmd /V /C "set NATTEN_N_WORKERS=8 && NATTEN_VERBOSE=1 && NATTEN_CUDA_ARCH=8.6 && python setup.py install"
+goto :installStart
+:end
+
+:installStart
+goto :parseargs
+:end
+
+:installFinalize
+set NATTEN_N_WORKERS=
+set NATTEN_CUDA_ARCH=
+set NATTEN_VERBOSE=
 goto :eof
+:end
+
+:installContinue
+echo NATTEN_N_WORKERS: %NATTEN_N_WORKERS%
+echo NATTEN_CUDA_ARCH: %NATTEN_CUDA_ARCH%
+echo NATTEN_VERBOSE: %NATTEN_VERBOSE%
+pip install -r requirements.txt
+python setup.py install
+goto :installFinalize
+:end
 
 :clean
 echo Cleaning up
 echo "Removing %CD%\build"
 del %CD%\build
 goto :eof
+:end
